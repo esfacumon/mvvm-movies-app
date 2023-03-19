f4a472207b88671943a4dff47bdd60d9b009d85a
fas <facundosierrar@gmail.com>
2023-03-19 19:56:17 +0100

    Github integration

--- app/src/main/java/com/example/wembleymovies/data/api/ApiConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.wembleymovies.data.api
-
-object ApiConstants {
-    const val URI = "https://api.themoviedb.org/3/"
-    const val API_KEY = "20220e44285cd3f62959f2945f75c693"
-    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
-    const val POSTER_SIZE = "w185"
-}
